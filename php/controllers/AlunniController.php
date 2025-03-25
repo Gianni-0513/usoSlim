1442b40e8492333444157ba8d20979e7540a6b91
lai2-24 <lai2-24@itismeucci.com>
2025-03-25 14:16:06 +0100

    finito

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -2,6 +2,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+
+
 class AlunniController
 {
   public function index(Request $request, Response $response, $args){
@@ -11,5 +13,62 @@ class AlunniController
 
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function show(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = " . $args['id']);
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody()->getContents(), true);
+    
+    
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome) VALUES ('" . $body['nome'] . "', '" . $body['cognome'] . "' )");
+    
+    if($result){
+      $response->getBody()->write(json_encode(["message" => "Alunno inserito"]));
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
+    }else{
+      $response->getBody()->write(json_encode(["message" => "Errore Inserimento"]));
+      return $response->withHeader("Content-type", "application/json")->withStatus(404);
+    }
+  }
+
+  public function update(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody()->getContents(), true);
+
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "UPDATE alunni SET nome = '" .$body['nome']. "', cognome = '" .$body['cognome']. "' WHERE id = " .$args['id'];
+    $result = $mysqli_connection->query($query);
+
+    if($result){
+      $response->getBody()->write(json_encode(["message" => "Alunno aggiornato"]));
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
+    }else{
+      $response->getBody()->write(json_encode(["message" => "ID non trovato"]));
+      return $response->withHeader("Content-type", "application/json")->withStatus(404);
+    }
+  }
+
+  public function destroy(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody()->getContents(), true);
+
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $query = "DELETE FROM alunni WHERE id = " .$args['id'];
+    $result = $mysqli_connection->query($query);
+
+    if($result){
+      $response->getBody()->write(json_encode(["message" => "Alunno eliminato"]));
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
+    }else{
+      $response->getBody()->write(json_encode(["message" => "Alunno non eliminato"]));
+      return $response->withHeader("Content-type", "application/json")->withStatus(404);
+    }
   }
 }
